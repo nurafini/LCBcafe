4c8bd17ca12877759b1e53a7c33c67f28a51a697
1626506 <finnabdwafiy@live.com>
2017-09-05 17:05:22 +0800

    Designing foundation of the website, adding navigation bar, webpages and completing a working contact page

---
+++ LCBcafewebsite/contact.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LCBCafe.Master" AutoEventWireup="true" CodeBehind="contact.aspx.cs" Inherits="LCBcafewebsite.contact" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    Name:<br />
+    <asp:TextBox ID="TextBoxName" runat="server"></asp:TextBox>
+    <br />
+    Email:<br />
+    <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+    <br />
+    Subject:<br />
+    <asp:TextBox ID="TextBoxSubject" runat="server"></asp:TextBox>
+    <br />
+    Message:<br />
+    <asp:TextBox ID="TextBoxMessage" runat="server"></asp:TextBox>
+    <br />
+    <asp:Literal ID="LiteralMessage" runat="server"></asp:Literal>
+    <br />
+    <asp:Button ID="ButtonSend" runat="server" Text="Send" OnClick="Button1_Click" />
+</asp:Content>
---
+++ LCBcafewebsite/contact.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LCBcafewebsite
+{
+    public partial class contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient client = new SmtpClient();
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+        
+            System.Net.NetworkCredential userpass = new System.Net.NetworkCredential("cafelcb@gmail.com", "cafelcb.");
+            client.UseDefaultCredentials = false;
+            client.Credentials = userpass;
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(TextBoxEmail.Text);
+            msg.To.Add(new MailAddress("cafelcb@gmail.com"));
+            msg.Subject = TextBoxSubject.Text;
+            msg.IsBodyHtml = true;
+            msg.Body = string.Format("From: " + TextBoxName.Text + ",   Email: " + TextBoxEmail.Text + ", Message: " + TextBoxMessage.Text);
+            try
+            {
+                client.Send(msg);
+                LiteralMessage.Text = "Thank you for contacting us!";
+            }
+            catch (Exception ex)
+            {
+                LiteralMessage.Text = "Fail to send message. Please try again." + ex.Message;
+            }
+            TextBoxName.Text = "";
+            TextBoxEmail.Text = "";
+            TextBoxSubject.Text = "";
+            TextBoxMessage.Text = "";
+        }
+    }
+}
